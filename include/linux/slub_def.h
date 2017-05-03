325217378e290585be993c8c689c0ae33f6ca72f
Daniel Micay <danielmicay@gmail.com>
2017-05-03 16:16:58 -0400

    slub: add multi-purpose random canaries
    From the configuration option:
    
    	Place canaries at the end of kernel slab allocations, sacrificing
    	some performance and memory usage for security.
    
    	Canaries can detect some forms of heap corruption when allocations
    	are freed and as part of the HARDENED_USERCOPY feature. It provides
    	basic use-after-free detection for HARDENED_USERCOPY.
    
    	Canaries absorb small overflows (rendering them harmless), mitigate
    	non-NUL terminated C string overflows on 64-bit via a guaranteed zero
    	byte and provide basic double-free detection.
    
    Signed-off-by: Daniel Micay <danielmicay@gmail.com>
    Signed-off-by: anupritaisno1 <www.anuprita804@gmail.com>

--- include/linux/slub_def.h
+++ include/linux/slub_def.h
@@ -94,6 +94,11 @@ struct kmem_cache {
 #endif
 #endif
 
+#ifdef CONFIG_SLAB_CANARY
+	unsigned long random_active;
+	unsigned long random_inactive;
+#endif
+
 #ifdef CONFIG_NUMA
 	/*
 	 * Defragmentation by allocating from a remote node.
